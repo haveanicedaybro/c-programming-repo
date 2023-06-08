5e759d940d334cd7608f34b7282d2fefaeea2be7
haveanicedaybro <nguyenthanhdattppt191103@gmail.com>
2023-06-08 23:00:47 +0700

    create file
    create file

---
+++ new-folder/datatype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    //numerical types
+    int i=3;    //so nguyen
+    double gpa=3.2; //so thuc
+    printf("%d %.2lf",i,gpa);   //'.2' la so chu so sau dau phay
+
+    //alphabetical types
+    char grade='A';     //chi lay duy nhat 1 ki tu
+    char book[]="atomic habits";//lay nhieu ki tu ke ca dau cach, tuy nhien khai bao thi van dung char di kem voi dau []
+    printf("%c %s",grade,book);
+
+
+}
---
+++ new-folder/variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    //bien char
+    char character[]="helloworld";
+    printf("hello %s",&character);
+
+
+    return 0;
+}
