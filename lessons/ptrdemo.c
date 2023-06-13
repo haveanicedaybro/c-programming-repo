969686a60997162f62c11ab6118201deb358d2a9
haveanicedaybro <nguyenthanhdattppt191103@gmail.com>
2023-06-14 00:33:33 +0700

    update file

--- lessons/ptrdemo.c
+++ lessons/ptrdemo.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 
 int main(){
-     int a = 5;
-    int* ptr;
+    /* int a = 5;
+    int *ptr;
     ptr = &a;
     printf("%d\n",ptr);
     printf("%d",*ptr);
-    return 0;
+    return 0;*/
+    int a=1026;//normal declaration
+    int *p;//pointer type declaration
+    p=&a;
+    printf("%d\n",p);
+    printf("%d\n",*p);
+    char* p0;//a char ptr declaration
+    p0=(char*)p;
+    printf("%d",*p0);
+
+
 }
